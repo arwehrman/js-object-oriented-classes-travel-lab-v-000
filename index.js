623e1df419f2326f2e3359ca740e41668e8f9341
Amanda Wehrman <arwehrman@gmail.com>
2018-01-21 18:39:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = startDate;
+  }
 
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
